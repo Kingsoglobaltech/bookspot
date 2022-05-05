586e4d9381626950d498d6161574f49d96cf9895
Kingsoglobaltech <87183329+Kingsoglobaltech@users.noreply.github.com>
2022-05-05 15:38:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
         <div className="flex flex-col w-full lg:w-1/2 justify-center items-start  px-6 tracking-wide">
              <h1 className="text-black text-2xl my-4">Stripy Zig Zag Jigsaw Pillow and Duvet Set</h1>
-             <a className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="/products">SHOP NOW *</a>
+             <a className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black"">SHOP NOW *</a>
 
         </div>
 
