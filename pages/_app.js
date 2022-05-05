230f17410d478727a3b421da0dca303b13a7916e
Kingsoglobaltech <87183329+Kingsoglobaltech@users.noreply.github.com>
2022-05-05 15:42:56 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
